313affa08c35df987ffe81df3462bd89385cdcaa
Mateen <makjavaprogrammer@gmail.com>
2024-04-09 01:40:09 +0530

    basic programms

---
+++ 1/helloworld.ino
@@ -0,0 +1,10 @@
+void setup()
+{
+    Serial.begin(9600);
+}
+
+void loop()
+{
+    Serial.println("mateen");
+    delay(1000);
+}
---
+++ 1/on_off.ino
@@ -0,0 +1,36 @@
+/*
+ * Created by ArduinoGetStarted.com
+ *
+ * This example code is in the public domain
+ *
+ * Tutorial page: https://arduinogetstarted.com/tutorials/arduino-serial-monitor
+ */
+
+void setup()
+{
+    Serial.begin(9600);
+    pinMode(LED_BUILTIN, OUTPUT); // set the digital pin as output:
+}
+
+void loop()
+{
+    if (Serial.available()) // if there is data comming
+    {
+        String command = Serial.readStringUntil('\n'); // read string until meet newline character
+
+        if (command == "ON")
+        {
+            digitalWrite(LED_BUILTIN, HIGH);    // turn on LED
+            Serial.println("LED is turned ON"); // send action to Serial Monitor
+        }
+        else if (command == "OFF")
+        {
+            digitalWrite(LED_BUILTIN, LOW);      // turn off LED
+            Serial.println("LED is turned OFF"); // send action to Serial Monitor
+        }
+        else
+        {
+            Serial.println("type either ON OR OFF"); // send action to Serial Monitor
+        }
+    }
+}
