53118b9bae52780fb42da72bf48de93a4a39f84c
Mateen <makjavaprogrammer@gmail.com>
2024-04-12 03:15:00 +0530

    multi line

---
+++ src/lcd/multi-line/lcd.ino
@@ -0,0 +1,29 @@
+#include <LiquidCrystal.h>
+
+// LiquidCrystal lcd(12, 11, 10, 5, 4, 3, 2);
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+void setup()
+{
+  lcd.begin(16,2);
+  
+
+}
+
+void loop() {
+  lcd.setCursor(0,0);
+  
+  lcd.print("Parvind");
+  delay(1000);
+
+  lcd.setCursor(0,1);
+  lcd.print("Sharma");
+  
+  delay(1000);
+  lcd.setCursor(0,0);
+  lcd.clear();
+  lcd.setCursor(0,1);
+  lcd.clear();
+  delay(1000);
+  
+  
+}
