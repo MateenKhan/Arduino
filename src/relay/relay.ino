5812dda27d0ffddfb02844c95d27dc3948d53c91
Mateen <makjavaprogrammer@gmail.com>
2024-04-21 01:42:35 +0530

    relay details added

---
+++ src/relay/relay.ino
@@ -0,0 +1,13 @@
+#define RELAY_PIN 2
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(RELAY_PIN, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(RELAY_PIN, LOW);
+  delay(2000);
+  digitalWrite(RELAY_PIN, HIGH);
+  delay(2000);
+}
