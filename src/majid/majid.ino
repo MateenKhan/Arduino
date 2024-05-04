4ed318c2f9e4ec557de0accd04838e61b80cb6d4
Mateen <makjavaprogrammer@gmail.com>
2024-05-04 21:40:00 +0530

    temp

--- src/majid/majid.ino
+++ src/majid/majid.ino
@@ -20,7 +20,9 @@ byte colPins[COLS] = {6, 7, 8, 9};
 Keypad customKeypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 int LED = 10;
 int relay = 11;
-
+int intput_volume = 0;
+int total_volume = 0;
+bool isFilling = false;
 
 void setup()
 {
@@ -38,41 +40,68 @@ void setup()
 
 void loop()
 {
-
-    char pressedKey = customKeypad.getKey();
-    if (pressedKey)
-    {
-        lcd.setCursor(10, 0);
-        lcd.print(pressedKey);
-
-        lcd.setCursor(10, 1);
-        lcd.print(pressedKey);
-
-        Serial.print(pressedKey);
-        if (pressedKey == 'D'){
-            digitalWrite(LED, HIGH);
-            turnOnPump();
-        } else{
-            digitalWrite(LED, LOW);
-            turnOffPump();
-        }
+    Serial.print(isFilling);
+    if(!isFilling){
+        Serial.print(isFilling);
+        processInput();
     }
-
     delay(200);
 }
 
+void processInput(){
+    char pressedKey = customKeypad.getKey();
+    if (pressedKey){
+        Serial.print(pressedKey);
+        if (pressedKey >= '0' && pressedKey <= '9'){
+            String str_input = "";
+            str_input += pressedKey;
+            int int_input = str_input.toInt();
+            if(intput_volume == 0){
+                intput_volume = int_input;
+            } else {
+                intput_volume = (intput_volume * 10) + int_input;
+            }
+        } else{
+            if (pressedKey == 'D'){
+                startFillingLiquid();
+            }
+        }
+
+    }
+}
+
+void startFillingLiquid(){
+    digitalWrite(LED, HIGH);
+    turnOnPump();
+    total_volume++;
+    delay(100);
+    if(intput_volume >= total_volume){
+        stopFillingLiquid();
+    }
+}
+
+void stopFillingLiquid(){
+    digitalWrite(LED, LOW);
+    turnOffPump();
+    delay(1000);
+    intput_volume = 0;
+    total_volume = 0 ;
+    
+}
 
 void turnOnPump(){
+    isFilling = true;
     digitalWrite(relay, LOW);
 }
 
 void turnOffPump(){
+    isFilling = false;
     digitalWrite(relay, HIGH);
 }
 
 void printConstantFillingInformation(){
     lcd.setCursor(0, 0);
-    lcd.print("Speed : ");
+    lcd.print("required : ");
 
     lcd.setCursor(14, 0);
     lcd.print("mL");
@@ -82,4 +111,12 @@ void printConstantFillingInformation(){
 
     lcd.setCursor(14, 1);
     lcd.print("mL");
+}
+
+void printVariableFillingInformation(){
+    lcd.setCursor(10, 0);
+    lcd.print(intput_volume);
+
+    lcd.setCursor(10, 1);
+    lcd.print(total_volume);
 }
